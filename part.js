c056d0fd5223c78b182447a2a4507bab21a55150
burgers6 <bryan.burgers@gmail.com>
2010-08-25 17:05:53 -0500

    Removed a few method from part.js, as well as the R90 part.

--- part.js
+++ part.js
@@ -38,24 +38,14 @@ var Part = (function() {
    return Utility.createResult(x, y, r);
   }
 
-  function r90Function(t) {
-   var theta = t * (Math.PI / 2);
-   var x = - HALF_SIZE * Math.cos(theta) + HALF_SIZE;
-   var y = - HALF_SIZE * Math.sin(theta) + HALF_SIZE;
-   var r = t * 90;
-   return Utility.createResult(x, y, r);
-  }
-
   var parts = {
     straight248: createPart("straight248", "248mm Straight Track", "#straight248", 248, straight248Function),
-    r90: createPart("r90", "90 degree curve to the right", "#r90", Math.PI * TRACK_SIZE / 4, r90Function),
     "curve45-249": createPart("curve45-249", "249mm radius 45 degree Curve Track", "#curve45_249", Math.PI * 249 / 4, c45_249Function)
   }
 
   function allParts() {
     return [
       parts.straight248,
-      parts.r90,
       parts.curve45_249
     ];
   }
@@ -64,21 +54,11 @@ var Part = (function() {
     return parts[id];
   }
 
-  function flip(part) {
-    return createPart(part.id, part.name, part.svgIdentifier, part.length, function(t) {
-      var result = part.f(1 - t);
-      return Utility.createResult(result.position.x, result.position.y, result.rotation + 180);
-    });
-  }
-
   return {
     allParts: allParts,
     fromId: fromId,
-    flip: flip,
 
     straight248: parts.straight248,
-    r90: parts.r90,
-    c45_249: parts["curve45-249"],
     curve45_249: parts["curve45-249"],
     "curve45-249": parts["curve45-249"]
   }
